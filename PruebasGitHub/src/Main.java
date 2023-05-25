b6c272747242dec1d8119ac0912454722de28bf5
DanielGSalcedo <134619621+DanielGSalcedo@users.noreply.github.com>
2023-05-25 15:50:20 -0500

    first test

---
+++ PruebasGitHub/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        //this si the main
+        String name = "daniel";
+        System.out.println("Hello world!");
+        System.out.println("Hey ".concat(name));
+    }
+}
