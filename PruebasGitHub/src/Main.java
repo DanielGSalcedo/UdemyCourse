0a65433d4af7ee22c534f8850bad1e6e7da184eb
DanielGSalcedo <134619621+DanielGSalcedo@users.noreply.github.com>
2023-05-25 16:04:11 -0500

    new version

--- PruebasGitHub/src/Main.java
+++ PruebasGitHub/src/Main.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //this si the main
-        String name = "daniel";
-        System.out.println("Hello world!");
-        System.out.println("Hey ".concat(name));
+        //this si the develop version
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Hey sup?, what's your name?");
+        String name = scan.nextLine();
+        System.out.println("Hey ".concat(name).concat(" what's your age ?"));
+        int age = scan.nextInt();
+
+        if (age < 18 ){
+            System.out.println("so, you are still a kid ");
+        }else {
+            System.out.println("so, you are an adult that's great");
+        }
     }
 }
