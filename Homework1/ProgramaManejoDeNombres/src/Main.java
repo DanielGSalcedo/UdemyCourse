aa3a957b9e1e9acff6078f66819fcad29b751d60
DanielGSalcedo <134619621+DanielGSalcedo@users.noreply.github.com>
2023-05-25 20:04:04 -0500

    Update Main.java

--- Homework1/ProgramaManejoDeNombres/src/Main.java
+++ Homework1/ProgramaManejoDeNombres/src/Main.java
@@ -10,10 +10,9 @@ public class Main {
         String name2 = scan.nextLine();
         System.out.println("ingrese el terccer nombre");
         String name3 = scan.nextLine();
-
+        
         System.out.println((name1.toUpperCase().charAt(1))+"."+name1.substring(name1.length()-2)+"_"
                 +(name2.toUpperCase().charAt(1))+"."+name2.substring(name2.length()-2)+"_"+(name3.toUpperCase().charAt(1))+"."
                 +name3.substring(name3.length()-2));
-
     }
 }
