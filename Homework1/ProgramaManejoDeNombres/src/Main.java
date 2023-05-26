c819e93dad93257b13fb1d7f238b091440790182
DanielGSalcedo <134619621+DanielGSalcedo@users.noreply.github.com>
2023-05-25 21:17:21 -0500

    sub dev v.1.0

--- Homework1/ProgramaManejoDeNombres/src/Main.java
+++ Homework1/ProgramaManejoDeNombres/src/Main.java
@@ -1,19 +1,17 @@
 import java.util.Scanner;
-//this is dev v.0.1
+//this is main v.1.0
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String result = "";
+        StringBuilder result = new StringBuilder();
         String name;
-
-        for (int i = 1; i <= 2; i++) {
+        for (int i = 1; i <= 3; i++) {
             System.out.println("ingrese el nombre numero ".concat(String.valueOf(i)));
             name = scan.nextLine();
-            result += ((name.toUpperCase().charAt(1)) + "." + name.substring(name.length() - 2) + "_");
+            String substring = name.substring(name.length() - 2);
+            result.append((i != 3) ? ((name.toUpperCase().charAt(1)) + "." + substring + "_") : "");
+            result.append((i == 3) ? ((name.toUpperCase().charAt(1)) + "." + substring) : "");
         }
-        System.out.println("ingrese el nombre numero 3");
-        name =scan.nextLine();
-        result += ((name.toUpperCase().charAt(1)) + "." + name.substring(name.length() - 2));
         System.out.println(result);
     }
 }
