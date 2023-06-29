db1d11579c7f7144f87c25e4c80d1faa6d72f65e
DanielGSalcedo <134619621+DanielGSalcedo@users.noreply.github.com>
2023-06-29 10:50:27 -0500

    name

--- Homework1/ProgramaManejoDeNombres/src/Main.java
+++ Homework1/ProgramaManejoDeNombres/src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+<<<<<<< Updated upstream
         StringBuilder result = new StringBuilder();
         String name;
         for (int i = 1; i <= 3; i++) {
@@ -11,6 +12,15 @@ public class Main {
             String substring = name.substring(name.length() - 2);
             result.append((i != 3) ? ((name.toUpperCase().charAt(1)) + "." + substring + "_") : "");
             result.append((i == 3) ? ((name.toUpperCase().charAt(1)) + "." + substring) : "");
+=======
+        String result ="";
+        //this is develop v.0.1
+
+        for( int i = 1;i<=3;i++){
+        System.out.println("ingrese el primer nombre");
+        String name = scan.nextLine();
+        result += ((name.toUpperCase().charAt(1))+"."+name.substring(name.length()-2)+"_");
+>>>>>>> Stashed changes
         }
         System.out.println(result);
     }
